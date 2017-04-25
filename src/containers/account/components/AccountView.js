60a6501904742ab8f602045a3e9a40c0d453328a
Corwin Crownover <corwin@thedataloom.com>
2017-04-25 13:23:18 -0700

    Fix auth proptype error

--- src/containers/account/components/AccountView.js
+++ src/containers/account/components/AccountView.js
@@ -9,9 +9,9 @@ import AccountDetails from '../containers/AccountDetails';
 import AccountOrganizations from '../containers/AccountOrganizations';
 
 // TODO:  Rename to NOT container
-export default class EditAccountView extends React.Component {
+export default class AccountView extends React.Component {
   static propTypes = {
-    auth: PropTypes.instanceOf(AuthService).isRequired
+    auth: PropTypes.instanceOf(AuthService)
   }
   render() {
     return (
